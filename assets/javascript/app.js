f4a471c3e539d3468b12c253d7e59fcb4d03dac2
deanmixer <deanmixer@users.noreply.github.com>
2019-01-05 22:43:59 -0500

    Project upload

---
+++ assets/javascript/app.js
@@ -0,0 +1,76 @@
+
+
+let tryitout = $(document).ready(function() {
+
+let buttonChoices = ["Adam West", "Michael Keaton", "Val Kilmer", "George Clooney", "Christian Bale", "Ben Affleck", "Will Arnett"];
+
+for(i = 0; i < buttonChoices.length; i++) {
+    let buttons = $('<button>'+ buttonChoices[i] + '</button>') 
+    buttons.addClass("btn btn-dark")
+    buttons.css('margin','2px');
+    buttons.attr("data-animal", buttonChoices[i]);
+    buttons.appendTo('#choices'); 
+    $("#choices").append(buttons);	
+}
+
+$("#add-Bat").on("click", function(event) {
+	event.preventDefault();
+	let userInput = $("#newBat").val().trim();
+    let buttons = $('<button>'+ userInput + '</button>') 
+    buttons.addClass("btn btn-dark")
+    buttons.css('margin','2px');
+    buttons.attr("data-animal", userInput);
+    buttons.appendTo('#choices'); 
+    $("#choices").append(buttons);	
+});
+
+$("#choices").on("click", ".btn-dark", function() {
+	$(".btn-dark").on("click", function() {
+	$("#gifs-appear-here").empty();
+	$(event.currentTarget).removeClass("btn btn-dark");
+	$(event.currentTarget).addClass("btn btn-warning");
+	$(event.currentTarget).siblings().removeClass("btn btn-warning");
+	$(event.currentTarget).siblings().addClass("btn btn-dark");
+      let actor = $(this).attr("data-animal");
+      console.log()
+      var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+        actor + "&api_key=83CMTH58dPC8tZpUHf2uWtUD0Tb2BRKC&limit=10";
+      $.ajax({
+        url: queryURL,
+        method: "GET"
+      })
+        .then(function(response) {
+          let results = response.data;
+
+          for (let i = 0; i < results.length; i++) {
+            let actorDiv = $("<div>");
+            let p = $("<p>").text("Rating: " + results[i].rating);
+            let actorImage = $("<img>");
+            actorImage.attr("src", results[i].images.fixed_height.url);
+            // actorImage.attr("data-state", "still");
+            actorImage.css('max-width','100%');
+            actorDiv.append(p);
+            actorDiv.append(actorImage);
+            $("#gifs-appear-here").prepend(actorDiv);
+          }
+        });
+    });
+     });   
+
+    $(".gif").on("click", function() {
+      // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+      var state = $(this).attr("data-state");
+      // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+      // Then, set the image's data-state to animate
+      // Else set src to the data-still value
+      if (state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+      } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+      }
+    });
+
+})
+
